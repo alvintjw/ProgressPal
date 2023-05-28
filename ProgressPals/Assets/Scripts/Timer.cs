3f6c2b1a48c139a33f94baa0e3f1b50ae26112d8
alvintjw <imalvin2@gmail.com>
2023-05-28 14:36:03 +0800

    Countdown timer working
    Time counts down properly, cirlce is unfilled properly and it say's time up when timer is over and change color of text ot red as intended

---
+++ ProgressPals/Assets/Scripts/Timer.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Image uiFill;
+    [SerializeField] private TextMeshProUGUI uiText;
+    [SerializeField] private TMP_InputField durationInput;
+    [SerializeField] private TextMeshProUGUI inputPromptText;  // Add a reference to the input prompt text
+    private Button startButton;
+
+    private int remainingDurationInSeconds;  // Represents the remaining duration in seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+        startButton = FindObjectOfType<Button>();
+        if (startButton != null)
+        {
+            startButton.onClick.AddListener(StartTimer);
+        }
+        else
+        {
+            Debug.LogError("Start Button not found in the scene.");
+        }
+    }
+    
+
+    private void StartTimer()
+    {
+  
+        int inputDuration;
+        if (int.TryParse(durationInput.text, out inputDuration))
+        {
+            startButton.gameObject.SetActive(false);  // Destroy the start button GameObject
+            durationInput.gameObject.SetActive(false);  // Destroy the duration input text GameObject
+            inputPromptText.gameObject.SetActive(false);  // Destroy the input prompt text GameObject
+            Begin(inputDuration);
+        
+        }
+        else
+        {
+            Debug.LogError("Invalid input duration.");
+        }
+    }
+
+    private void Begin(int minutes)
+    {
+        Debug.Log("Begin method called.");
+        remainingDurationInSeconds = minutes * 60;  // Convert minutes to seconds
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+
+        float initialDuration = remainingDurationInSeconds; 
+        while (remainingDurationInSeconds > 0)
+        {
+            int minutes = remainingDurationInSeconds / 60;  // Get the minutes
+            int seconds = remainingDurationInSeconds % 60;  // Get the seconds
+            uiText.text = $"{minutes:00} : {seconds:00}";
+        
+            uiFill.fillAmount = Mathf.InverseLerp(0, initialDuration, remainingDurationInSeconds);
+            remainingDurationInSeconds--;
+            yield return new WaitForSeconds(1f);
+        }
+        OnEnd();
+
+
+    }
+    private void OnEnd()
+    {
+        uiText.text = "Time's up";
+        uiFill.fillAmount = 0f;
+        uiText.color = Color.red;
+    }
+}
