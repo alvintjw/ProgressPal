16edae2d87ef15433b9d5ca47048d3656132732e
alvintjw <imalvin2@gmail.com>
2023-06-22 17:25:51 +0800

    Cancel Function
    user can start task, pause timer and stop timer

--- ProgressPalsV2/ProjectPalsV2/Assets/Scripts/Timer.cs
+++ ProgressPalsV2/ProjectPalsV2/Assets/Scripts/Timer.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -7,25 +6,20 @@ using UnityEngine.EventSystems;
 
 public class Timer : MonoBehaviour, IPointerClickHandler
 {
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Pause = !Pause;
-    } 
-    
     [SerializeField] private Image uiFill;
     [SerializeField] private TextMeshProUGUI uiText;
     [SerializeField] private Slider durationSlider;
-    //[SerializeField] private TextMeshProUGUI inputPromptText;
     public Button startButton;
-    private bool Pause;
 
+    private bool isTimerRunning;
+    private bool isPaused;
     private int remainingDurationInSeconds;
 
-    void Start()
+    private void Start()
     {
         if (startButton != null)
         {
-            startButton.onClick.AddListener(StartTimer);
+            startButton.onClick.AddListener(ToggleTimer);
         }
         else
         {
@@ -34,21 +28,71 @@ public class Timer : MonoBehaviour, IPointerClickHandler
 
         durationSlider.onValueChanged.AddListener(UpdateSliderText);
     }
-    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isTimerRunning)
+        {
+            TogglePause();
+        }
+    }
+
+    private void ToggleTimer()
+    {
+        if (isTimerRunning)
+        {
+            StopTimer();
+        }
+        else
+        {
+            StartTimer();
+        }
+    }
+
     private void StartTimer()
     {
         float sliderValue = durationSlider.value;
         int inputDuration = Mathf.RoundToInt(sliderValue * 60);  // Convert slider value to minutes
 
-        startButton.gameObject.SetActive(false);
+        isTimerRunning = true;
+        startButton.GetComponentInChildren<TextMeshProUGUI>().text = "Stop Timer";
         durationSlider.gameObject.SetActive(false);
-        //inputPromptText.gameObject.SetActive(false);
+
         Begin(inputDuration);
+    }
+
+    private void StopTimer()
+    {
+        isTimerRunning = false;
+        isPaused = false;
+        startButton.GetComponentInChildren<TextMeshProUGUI>().text = "Start Timer";
+        durationSlider.gameObject.SetActive(true);
+        ResetUI();
+        StopAllCoroutines();
+    }
+
+    private void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            startButton.GetComponentInChildren<TextMeshProUGUI>().text = "Stop Timer";
+        }
+        else
+        {
+            startButton.GetComponentInChildren<TextMeshProUGUI>().text = "Stop Timer";
+        }
+    }
+
+    private void ResetUI()
+    {
+        uiText.text = "00:00";
+        uiFill.fillAmount = 0f;
+        uiText.color = Color.white;
     }
 
     private void Begin(int seconds)
     {
-        Debug.Log("Begin method called.");
         remainingDurationInSeconds = seconds;
         StartCoroutine(UpdateTimer());
     }
@@ -56,33 +100,34 @@ public class Timer : MonoBehaviour, IPointerClickHandler
     private IEnumerator UpdateTimer()
     {
         float initialDuration = remainingDurationInSeconds;
-        
+
         while (remainingDurationInSeconds > 0)
         {
-            if (!Pause)
+            if (isTimerRunning && !isPaused)
             {
                 int minutes = remainingDurationInSeconds / 60;
                 int seconds = remainingDurationInSeconds % 60;
-                uiText.text = $"{minutes:00} : {seconds:00}";
+                uiText.text = $"{minutes:00}:{seconds:00}";
                 uiFill.fillAmount = Mathf.InverseLerp(0, initialDuration, remainingDurationInSeconds);
                 remainingDurationInSeconds--;
-                yield return new WaitForSeconds(1f);
             }
-            yield return null;
+            yield return new WaitForSeconds(1f);
         }
+
         OnEnd();
     }
 
     private void OnEnd()
     {
+        isTimerRunning = false;
+        startButton.GetComponentInChildren<TextMeshProUGUI>().text = "Start Timer";
+        durationSlider.interactable = true;
+
         uiText.text = "Time's up";
         uiFill.fillAmount = 0f;
         uiText.color = Color.red;
-        startButton.gameObject.SetActive(true);
-        durationSlider.gameObject.SetActive(true);
-        //inputPromptText.gameObject.SetActive(true);
     }
-    
+
     private void UpdateSliderText(float value)
     {
         int totalSeconds = Mathf.RoundToInt(value * 60);
