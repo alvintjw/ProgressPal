9f1b78b2d3446c9d1b8f20228b3ebdb4c82ddbbb
alvintjw <imalvin2@gmail.com>
2023-06-26 01:11:49 +0800

    Final Final Timer Page for milestone 2

--- ProgressPalsV2/ProjectPalsV2/Assets/Scripts/ScenesManager.cs
+++ ProgressPalsV2/ProjectPalsV2/Assets/Scripts/ScenesManager.cs
@@ -14,4 +14,9 @@ public class ScenesManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public void TaskPageButton()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
